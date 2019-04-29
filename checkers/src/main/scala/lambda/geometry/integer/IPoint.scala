d614dae4d93a6d86aae85946a93bd27203f24bf6
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-04-29 11:20:35 +0800

    Cleanup

--- checkers/src/main/scala/lambda/geometry/integer/IPoint.scala
+++ checkers/src/main/scala/lambda/geometry/integer/IPoint.scala
@@ -7,5 +7,11 @@ import lambda.geometry.floating.FPoint
   */
 case class IPoint(x: Int, y: Int) {
 
+  def +(p: IPoint) = IPoint(x + p.x, y + p.y)
+
+  def *(d: Int) = IPoint(x * d, y * d)
+
+  def /(d: Int) = IPoint(x / d, y / d)
+
   def toFPoint : FPoint = FPoint(x, y)
 }
