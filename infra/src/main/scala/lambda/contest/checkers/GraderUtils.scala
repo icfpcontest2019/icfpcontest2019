073b64456abce3f0df53e4d2390327cc43ea4f1c
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-05-23 21:11:02 +0800

    Correctly loading matrices in the grader

--- infra/src/main/scala/lambda/contest/checkers/GraderUtils.scala
+++ infra/src/main/scala/lambda/contest/checkers/GraderUtils.scala
@@ -24,15 +24,15 @@ object GraderUtils {
   val BOOSTERS_EXT = ".buy"
 
   type TaskDescription = (TaskMatrix, Int, Int, IPoint)
-  
+
   def getTaskNumbers(path: String): List[Int] = {
     val dir = new File(path)
     if (!dir.isDirectory)
       throw ContestException(s"File ${dir.getAbsolutePath} is not a directory.")
     val nums = for (f <- dir.listFiles();
-         fname = f.getName
-         if fname.startsWith(PROBLEM_PREFIX) && fname.endsWith(PROBLEM_DESC_EXT);
-         tNum = fname.stripPrefix(PROBLEM_PREFIX).stripSuffix(PROBLEM_DESC_EXT).toInt) 
+                    fname = f.getName
+                    if fname.startsWith(PROBLEM_PREFIX) && fname.endsWith(PROBLEM_DESC_EXT);
+                    tNum = fname.stripPrefix(PROBLEM_PREFIX).stripSuffix(PROBLEM_DESC_EXT).toInt)
       yield tNum
     nums.toList.sorted
   }
@@ -42,10 +42,10 @@ object GraderUtils {
     if (!mainDir.isDirectory)
       throw ContestException(s"File ${mainDir.getAbsolutePath} is not a directory.")
     // First try to get as a matrix
-    
+
     val subDirs = mainDir.listFiles().toList.filter(f => f.isDirectory && f.getName != MATRIX_FOLDER)
     val allDirs = mainDir :: subDirs
-    
+
     getTaskMatrix(allDirs, solution) match {
       case res@Some(_) => res
       case None =>
@@ -66,22 +66,18 @@ object GraderUtils {
 
 
   def getTaskMatrix(allDirs: List[File], num: Int): Option[TaskDescription] = {
-    allDirs
+    val allMatrixFolders = allDirs
       .flatMap(_.listFiles().toList)
-      .find(f => f.isDirectory && f.getName == MATRIX_FOLDER) match {
-      case None => None
-      case Some(mdir) =>
-        for (f <- mdir.listFiles();
-             fname = f.getName
-             if fname.startsWith(PROBLEM_PREFIX) && fname.endsWith(PROBLEM_MATRIX_EXT);
-             fNum = fname.stripPrefix(PROBLEM_PREFIX).stripSuffix(PROBLEM_MATRIX_EXT).toInt
-             if num == fNum) {
-          val ls = FileUtil.readFromFile(f.getAbsolutePath)
-          val td = stringsToTaskMatrix(ls)
-          return Some(td)
-        }
+      .filter(f => f.isDirectory && f.getName == MATRIX_FOLDER)
+    for (f <- allMatrixFolders.flatMap(_.listFiles().toList.sorted);
+         fname = f.getName
+         if fname.startsWith(PROBLEM_PREFIX) && fname.endsWith(PROBLEM_MATRIX_EXT);
+         fNum = fname.stripPrefix(PROBLEM_PREFIX).stripSuffix(PROBLEM_MATRIX_EXT).toInt
+         if num == fNum) {
+      val ls = FileUtil.readFromFile(f.getAbsolutePath)
+      val td = stringsToTaskMatrix(ls)
+      return Some(td)
     }
-
     None
   }
 
