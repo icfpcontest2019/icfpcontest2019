4a2843ae77eb254778f029959ba11a479592fabe
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-05-24 16:53:54 +0800

    refactorings

--- core/src/main/scala/lambda/geometry/integer/JPolygon.scala
+++ core/src/main/scala/lambda/geometry/integer/JPolygon.scala
@@ -9,13 +9,13 @@ case class JPolygon(xpoints: Array[Int], ypoints: Array[Int], npoints: Int,
   def contains(x: Double, y: Double): Boolean = {
     import util.control.Breaks._
 
-    if (npoints <= 2 || !boundingBox.contains(x, y) ) return false
+    if (npoints <= 2 || !boundingBox.contains(x, y)) return false
     var hits = 0
     var lastx = xpoints(npoints - 1)
     var lasty = ypoints(npoints - 1)
     var curx = 0
     var cury = 0
-    
+
     // Walk the edges of the polygon
     var i = 0
 
@@ -26,61 +26,70 @@ case class JPolygon(xpoints: Array[Int], ypoints: Array[Int], npoints: Int,
       break
     }
 
+    def tick(): Unit = {
+      lastx = curx
+      lasty = cury
+      i = i + 1
+    }
+
     while (i < npoints) {
       breakable {
         curx = xpoints(i)
         cury = ypoints(i)
         if (cury == lasty) {
-          breakOut()
-        }
-        var leftx = 0
-        if (curx < lastx) {
-          if (x >= lastx) {
-            breakOut()
-          }
-          leftx = curx
+          tick()
         } else {
-          if (x >= curx) {
-            breakOut()
+          var leftx = 0
+          if (curx < lastx) {
+            if (x >= lastx) {
+              breakOut()
+            }
+            leftx = curx
+          } else {
+            if (x >= curx) {
+              breakOut()
+            }
+            leftx = lastx
           }
-          leftx = lastx
+          var test1 = .0
+          var test2 = .0
+          if (cury < lasty) {
+            if (y < cury || y >= lasty) {
+              tick()
+            } else if (x < leftx) {
+              hits += 1
+              tick()
+            } else {
+              test1 = x - curx
+              test2 = y - cury
+              if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
+                hits += 1
+              }
+              tick()
+            }
+          } else {
+            if (y < lasty || y >= cury) {
+              breakOut()
+            }
+            if (x < leftx) {
+              hits += 1
+              breakOut()
+            }
+            test1 = x - lastx
+            test2 = y - lasty
+            if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
+              hits += 1
+            }
+            tick()
+          }
+
         }
-        var test1 = .0
-        var test2 = .0
-        if (cury < lasty) {
-          if (y < cury || y >= lasty) {
-            breakOut()
-          }
-          if (x < leftx) {
-            hits += 1
-            breakOut()
-          }
-          test1 = x - curx
-          test2 = y - cury
-        }
-        else {
-          if (y < lasty || y >= cury) {
-            breakOut()
-          }
-          if (x < leftx) {
-            hits += 1
-            breakOut()
-          }
-          test1 = x - lastx
-          test2 = y - lasty
-        }
-        if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
-          hits += 1
-        }
-        lastx = curx
-        lasty = cury
-        i = i + 1
       }
     }
-    
+
     (hits & 1) != 0
   }
-  
+
 }
 
 case class JRectangle(x: Int, y: Int, width: Int, height: Int) {
