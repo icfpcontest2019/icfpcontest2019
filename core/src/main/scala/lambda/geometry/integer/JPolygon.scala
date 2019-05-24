02f980760e5d5c80dd55a2fa63c7b540b87444ad
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-05-24 17:04:07 +0800

    Replacing java awt polygon with Scala implementation.

--- core/src/main/scala/lambda/geometry/integer/JPolygon.scala
+++ core/src/main/scala/lambda/geometry/integer/JPolygon.scala
@@ -1,14 +1,14 @@
 package lambda.geometry.integer
 
 /**
+  * Fast implementation adapted from Java AWT
+  * 
   * @author Ilya Sergey
   */
 case class JPolygon(xpoints: Array[Int], ypoints: Array[Int], npoints: Int,
                     boundingBox: JRectangle) {
 
   def contains(x: Double, y: Double): Boolean = {
-    import util.control.Breaks._
-
     if (npoints <= 2 || !boundingBox.contains(x, y)) return false
     var hits = 0
     var lastx = xpoints(npoints - 1)
@@ -19,13 +19,6 @@ case class JPolygon(xpoints: Array[Int], ypoints: Array[Int], npoints: Int,
     // Walk the edges of the polygon
     var i = 0
 
-    def breakOut() {
-      lastx = curx
-      lasty = cury
-      i = i + 1
-      break
-    }
-
     def tick(): Unit = {
       lastx = curx
       lasty = cury
@@ -33,58 +26,61 @@ case class JPolygon(xpoints: Array[Int], ypoints: Array[Int], npoints: Int,
     }
 
     while (i < npoints) {
-      breakable {
         curx = xpoints(i)
         cury = ypoints(i)
         if (cury == lasty) {
           tick()
         } else {
           var leftx = 0
-          if (curx < lastx) {
-            if (x >= lastx) {
-              breakOut()
-            }
-            leftx = curx
-          } else {
-            if (x >= curx) {
-              breakOut()
-            }
-            leftx = lastx
-          }
-          var test1 = .0
-          var test2 = .0
-          if (cury < lasty) {
-            if (y < cury || y >= lasty) {
-              tick()
-            } else if (x < leftx) {
-              hits += 1
+          
+          def tail1(): Unit = {
+            var test1 = .0
+            var test2 = .0
+            if (cury < lasty) {
+              if (y < cury || y >= lasty) {} 
+              else if (x < leftx) {
+                hits += 1
+              } else {
+                test1 = x - curx
+                test2 = y - cury
+                if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
+                  hits += 1
+                }
+              }
               tick()
             } else {
-              test1 = x - curx
-              test2 = y - cury
-              if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
+              if (y < lasty || y >= cury) {} 
+              else if (x < leftx) {
                 hits += 1
+              } else {
+                test1 = x - lastx
+                test2 = y - lasty
+                if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
+                  hits += 1
+                }
               }
               tick()
             }
-          } else {
-            if (y < lasty || y >= cury) {
-              breakOut()
-            }
-            if (x < leftx) {
-              hits += 1
-              breakOut()
-            }
-            test1 = x - lastx
-            test2 = y - lasty
-            if (test1 < (test2 / (lasty - cury) * (lastx - curx))) {
-              hits += 1
-            }
-            tick()
           }
-
+          
+          // Don't touch stuff below
+          if (curx < lastx) {
+            if (x >= lastx) {
+              tick()
+            } else {
+              leftx = curx
+              tail1()
+            }
+          } else {
+            if (x >= curx) {
+              tick()
+            } else {
+              leftx = lastx
+              tail1()
+            }
+          }
+          
         }
-      }
     }
 
     (hits & 1) != 0
